3d78d3ae828beba1f29bbe56591f2b6d5da3c065
Vinicius Fernandes <viniciusafx@outlook.com>
2023-11-24 13:14:05 -0300

    added cmp-cmdline plugin

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -24,3 +24,19 @@ cmp.setup {
         { name = 'buffer' },
     }),
 }
+
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' },
+    },
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' },
+    }, {
+        { name = 'cmdline' },
+    }),
+})
--- init.vim
+++ init.vim
@@ -81,6 +81,7 @@ Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
 
 Plug 'hrsh7th/cmp-vsnip'
